ff5056e185c65d0007c55c7a37d2c36a216d0359
Saru <154934036+tbsjub@users.noreply.github.com>
2025-08-19 15:31:44 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import createPage from './api/createPage.js';
 import getPage from './api/getPage.js';
 import parseTable from './data/htmlToJSON.js';
 import updatePage from './api/updatePage.js';
+import getData from './api/getData.js'
 import { readFileSync, writeFileSync } from 'fs';
 import ejs from 'ejs';
 
@@ -11,19 +12,17 @@ const pageId = 1682210847;
 // const rendered = ejs.render(template, {})
 const newTitle = 'The SHOT';
 
-// Async function to handle the updatePage call
-async function main() {
-  try {
-    await updatePage(pageId, newTitle, { 
-      template: './templates/putPageContent.ejs', 
-      data: {title: "Test"} 
-    });
-  }
-}
-
-// Run the main function
-main();
-
 // const template = readFileSync('./templates/putPageContent.ejs', 'utf-8');
 // const rendered = ejs.render(template, {title: null}); // {} = your data object
 // writeFileSync('./ejsRender.html', rendered, 'utf-8');
+
+
+
+
+const pv = "L3-PSS:STATE_EXH_EXTERNAL_HIGH_P";
+const timeSpan = "1d";
+const outputFile = "data/csvFile.csv";
+const pemPath = "c://Users//sarita.pokhrel//Downloads//geant_issue.pem";
+
+
+getData({pv, timeSpan, outputFile, pemPath});
